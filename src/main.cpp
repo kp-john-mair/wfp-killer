4749f99aa87706370c4d44d5eb588148c81dd5ee
John Mair <john.mair@kape.com>
2024-05-05 11:06:36 +0200

    Fix command arg parsing by removing parse() call in main

--- src/main.cpp
+++ src/main.cpp
@@ -61,17 +61,17 @@ int main(int argc, char** argv)
 
     try
     {
-        options.add_options()
-            ("h,help", "Display this help message.")
-            ("l,list", "List all filters across all layers (also accepts an optional partial name match for a provider or 'all').", cxxopts::value<std::vector<std::string>>())
-            ("m,monitor", "Monitor WFP events.")
-            ("p,provider", "Limit output to a specific provider (accepts partial name match).", cxxopts::value<std::vector<std::string>>()->default_value({}))
-            ("L,layer", "Limit output to a specific layer (accepts partial name match).", cxxopts::value<std::vector<std::string>>()->default_value({}))
-            ("s,sublayer", "Limit output to a specified sublayer (accepts partial name match).", cxxopts::value<std::vector<std::string>>()->default_value({}))
-            ("d,delete", "Delete a filter or all filters (takes a filter ID or 'all').", cxxopts::value<std::vector<std::string>>())
-            ("c,create", "Create a dummy filter for testing");
+        // options.add_options()
+        //     ("h,help", "Display this help message.")
+        //     ("l,list", "List all filters across all layers (also accepts an optional partial name match for a provider or 'all').", cxxopts::value<std::vector<std::string>>())
+        //     ("m,monitor", "Monitor WFP events.")
+        //     ("p,provider", "Limit output to a specific provider (accepts partial name match).", cxxopts::value<std::vector<std::string>>()->default_value({}))
+        //     ("L,layer", "Limit output to a specific layer (accepts partial name match).", cxxopts::value<std::vector<std::string>>()->default_value({}))
+        //     ("s,sublayer", "Limit output to a specified sublayer (accepts partial name match).", cxxopts::value<std::vector<std::string>>()->default_value({}))
+        //     ("d,delete", "Delete a filter or all filters (takes a filter ID or 'all').", cxxopts::value<std::vector<std::string>>())
+        //     ("c,create", "Create a dummy filter for testing");
 
-        auto result = options.parse(argc, argv);
+        // auto result = options.parse(argc, argv);
 
         if(argc < 2)
         {
@@ -91,63 +91,51 @@ int main(int argc, char** argv)
             return 1;
         }
 
-        if(commandName == "list")
-        {
-            std::cout << "got a list!";
-            //wfpk::ListCommand{std::move(pWfpKiller)}.run(argc, argv);
-            return 0;
-        }
-        else
-        {
-            std::cerr << "didn't get a list!";
-            return 1;
-        }
+        // if(result.count("create"))
+        // {
+        //     std::cout << "Trying to create a dummy callout filter\n";
+        //   //  pWfpKiller->createFilter();
+        //     return 0;
+        // }
 
-        if(result.count("create"))
-        {
-            std::cout << "Trying to create a dummy callout filter\n";
-          //  pWfpKiller->createFilter();
-            return 0;
-        }
+        // else if(result.count("help"))
+        // {
+        //   std::cout << options.help();
+        //   return 0;
+        // }
+        // else if(result.count("delete"))
+        // {
+        //     const auto &filterIdStrs = result["delete"].as<std::vector<std::string>>();
 
-        else if(result.count("help"))
-        {
-          std::cout << options.help();
-          return 0;
-        }
-        else if(result.count("delete"))
-        {
-            const auto &filterIdStrs = result["delete"].as<std::vector<std::string>>();
+        //     // Whether we should delete all filters (did the user pass in '-d all' ?)
+        //     bool shouldDeleteAllFilters = std::ranges::find(filterIdStrs, "all") != filterIdStrs.end();
 
-            // Whether we should delete all filters (did the user pass in '-d all' ?)
-            bool shouldDeleteAllFilters = std::ranges::find(filterIdStrs, "all") != filterIdStrs.end();
+        //     std::vector<wfpk::FilterId> filterIds;
+        //     filterIds.reserve(result.count("delete"));
 
-            std::vector<wfpk::FilterId> filterIds;
-            filterIds.reserve(result.count("delete"));
+        //     // Empty filterIds vector means we delete all filters
+        //     // If we shouldn't delete all - then we fill it with ids
+        //     if(!shouldDeleteAllFilters)
+        //     {
+        //         filterIds.reserve(filterIdStrs.size());
+        //         for(const auto &filterIdStr : filterIdStrs)
+        //             filterIds.push_back(std::stoull(filterIdStr));
+        //     }
 
-            // Empty filterIds vector means we delete all filters
-            // If we shouldn't delete all - then we fill it with ids
-            if(!shouldDeleteAllFilters)
-            {
-                filterIds.reserve(filterIdStrs.size());
-                for(const auto &filterIdStr : filterIdStrs)
-                    filterIds.push_back(std::stoull(filterIdStr));
-            }
+        //     //pWfpKiller->deleteFilters(filterIds);
 
-            //pWfpKiller->deleteFilters(filterIds);
-
-            return 0;
-        }
-        else if(result.count("monitor"))
-        {
-            //pWfpKiller->monitor();
-        }
-        else
-        {
-            std::cout << "Options are required.\n\n";
-            std::cout << options.help();
-            return 1;
-        }
+        //     return 0;
+        // }
+        // else if(result.count("monitor"))
+        // {
+        //     //pWfpKiller->monitor();
+        // }
+        // else
+        // {
+        //     std::cout << "Options are required.\n\n";
+        //     std::cout << options.help();
+        //     return 1;
+        // }
     }
     catch(const wfpk::WfpError& ex)
     {
